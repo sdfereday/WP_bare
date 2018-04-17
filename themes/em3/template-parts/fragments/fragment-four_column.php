0ee62cd647702e62de0f6fb0bf67e183656af2d6
Sam Fereday <sfereday@gmail.com>
2018-04-17 23:00:45 +0100

    Added extra helpers and removed some unrequired styles

---
+++ themes/em3/template-parts/fragments/fragment-four_column.php
@@ -0,0 +1,11 @@
+<div class="four-columns">
+    <?php
+    foreach( $row_data as $field_key => $item )
+    {
+        $field = get_field_object($field_key);
+        if($field) {
+            echo '<div class='.$field['name'].'>' . get_sub_field($field['name']) . '</div>';
+        }
+    }
+    ?>
+</div>
